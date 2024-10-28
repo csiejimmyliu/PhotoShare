4c7d80aef8bce9b13347d2e2d964fc02e1b85d83
csiejimmyliu <91661606+csiejimmyliu@users.noreply.github.com>
2024-10-28 15:39:12 +0800

    xcode 15.3/ ios 17.6

---
+++ PhotoActivity/PhotoActivityBundle.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoActivityBundle.swift
+//  PhotoActivity
+//
+//  Created by Jimmy Liu on 2024/10/28.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct PhotoActivityBundle: WidgetBundle {
+    var body: some Widget {
+        PhotoActivity()
+        PhotoActivityControl()
+        PhotoActivityLiveActivity()
+    }
+}
---
+++ PhotoActivity/PhotoActivityLiveActivity.swift
@@ -0,0 +1,77 @@
+//
+//  PhotoActivityLiveActivity.swift
+//  PhotoActivity
+//
+//  Created by Jimmy Liu on 2024/10/28.
+//
+
+import ActivityKit
+import WidgetKit
+import SwiftUI
+
+struct PhotoActivityAttributes: ActivityAttributes {
+    public struct ContentState: Codable, Hashable {
+        // Dynamic stateful properties about your activity go here!
+        var emoji: String
+    }
+
+    // Fixed non-changing properties about your activity go here!
+    var name: String
+}
+
+struct PhotoActivityLiveActivity: Widget {
+    var body: some WidgetConfiguration {
+        ActivityConfiguration(for: PhotoActivityAttributes.self) { context in
+            // Lock screen/banner UI goes here
+            VStack {
+                Text("Hello \(context.state.emoji)")
+            }
+            .activityBackgroundTint(Color.cyan)
+            .activitySystemActionForegroundColor(Color.black)
+
+        } dynamicIsland: { context in
+            DynamicIsland {
+                // Expanded UI goes here.  Compose the expanded UI through
+                // various regions, like leading/trailing/center/bottom
+                DynamicIslandExpandedRegion(.leading) {
+                    Text("Leading")
+                }
+                DynamicIslandExpandedRegion(.trailing) {
+                    Text("Trailing")
+                }
+                DynamicIslandExpandedRegion(.bottom) {
+                    Text("Bottom \(context.state.emoji)")
+                    // more content
+                }
+            } compactLeading: {
+                Text("L")
+            } compactTrailing: {
+                Text("T \(context.state.emoji)")
+            } minimal: {
+                Text(context.state.emoji)
+            }
+            .widgetURL(URL(string: "http://www.apple.com"))
+            .keylineTint(Color.red)
+        }
+    }
+}
+
+struct PhotoActivityLiveActivity_Previews:PreviewProvider {
+    static let attributes = PhotoActivityAttributes(name:"testing")
+    static let contentState = PhotoActivityAttributes.ContentState(emoji:"🤩")
+    
+    static var previews: some View {
+        attributes
+            .previewContext(contentState, viewKind: .dynamicIsland(.compact))
+            .previewDisplayName("Island Compact")
+        attributes
+            .previewContext(contentState, viewKind: .dynamicIsland(.expanded))
+            .previewDisplayName("Island Expanded")
+        attributes
+            .previewContext(contentState, viewKind: .dynamicIsland(.minimal))
+            .previewDisplayName("Minimal")
+        attributes
+            .previewContext(contentState, viewKind: .content)
+            .previewDisplayName("Notification")
+    }
+}
