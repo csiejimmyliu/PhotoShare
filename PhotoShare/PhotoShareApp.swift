8c7d539d70e668c098af1ce5ca4a7484c6ae720b
csiejimmyliu <91661606+csiejimmyliu@users.noreply.github.com>
2024-10-28 15:30:37 +0800

    Initial Commit

---
+++ PhotoShare/PhotoShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoShareApp.swift
+//  PhotoShare
+//
+//  Created by Jimmy Liu on 2024/10/28.
+//
+
+import SwiftUI
+
+@main
+struct PhotoShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
